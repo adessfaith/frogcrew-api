7bc629192ebd1ab78e8c3973f2ec99b98a1378e4
adessfaith <adessasegura@gmail.com>
2025-03-22 14:14:01 -0500

    Issue #1: Spring Boot project setup with H2 database config

---
+++ src/main/java/com/frogcrew/frogcrewapi/FrogcrewApiApplication.java
@@ -0,0 +1,13 @@
+package com.frogcrew.frogcrewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogcrewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogcrewApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frogcrew/frogcrewapi/FrogcrewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frogcrew.frogcrewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogcrewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
